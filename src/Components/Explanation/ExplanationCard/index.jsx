88c0ebe63f6b92754f29c44acbae00fd0f434387
Daniel Guinter <guinterdaniel@gmail.com>
2023-01-13 22:30:49 -0300

    Completed explanation card

---
+++ src/Components/Explanation/ExplanationCard/index.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+export default function ExplanationCard() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Através deste APP vocÊ vai consolidar {"\n"} 4 hábitos de áreas
+        fundamentais:
+      </Text>
+      <View style={styles.explanationContainer}>
+        <Image
+          source={require("../../../assets/icons/educationIcon.png")}
+          style={styles.icon}
+        />
+        <Text style={styles.description}>
+          <Text style={styles.mind}>Mente:</Text> Hábitos para melhorar sua
+          inteligência/sabedoria.
+        </Text>
+      </View>
+
+      <View style={styles.explanationContainer}>
+        <Image
+          source={require("../../../assets/icons/moneyIcon.png")}
+          style={styles.icon}
+        />
+        <Text style={styles.description}>
+          <Text style={styles.money}>Finança: </Text> hbitos para te ajudar no
+          controle financeiro.
+        </Text>
+      </View>
+
+      <View style={styles.explanationContainer}>
+        <Image
+          source={require("../../../assets/icons/bodyIcon.png")}
+          style={styles.icon}
+        />
+        <Text style={styles.description}>
+          <Text style={styles.body}>Corpo:</Text> Hábitos para te deixar mais
+          saudável e forte.
+        </Text>
+      </View>
+
+      <View style={styles.explanationContainer}>
+        <Image
+          source={require("../../../assets/icons/funIcon.png")}
+          style={styles.icon}
+        />
+        <Text style={styles.description}>
+          <Text style={styles.fun}>Humor:</Text> Hábitos para controlar o stress{" "}
+          {"\n"} e aumentar a felicidade.
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#151515",
+    width: 350,
+    borderRadius: 25,
+    padding: 30,
+  },
+
+  title: {
+    fontWeigth: "bold",
+    color: "white",
+    textAlign: "center",
+    fontSize: 16,
+  },
+
+  explanationContainer: {
+    flexDirection: "row",
+    marginTop: 30,
+  },
+
+  icon: {
+    width: 40,
+    height: 40,
+    marginRight: 5,
+  },
+
+  mind: {
+    color: "#98B7F3",
+    fontWeigth: "bold",
+  },
+
+  money: {
+    color: "#85BB65",
+    fontWeigth: "bold",
+  },
+
+  body: {
+    color: "#FF0044",
+    fontWeigth: "bold",
+  },
+
+  fun: {
+    color: "#FE7F23",
+    fontWeigth: "bold",
+  },
+
+  description: {
+    color: "white",
+  },
+});
--- src/Pages/AppExplanation/index.jsx
+++ src/Pages/AppExplanation/index.jsx
@@ -1,36 +1,33 @@
 import React from "react";
-import {
-    ScrollView,
-    View,
-    Text,
-    StyleSheet,
-    handleNavHome,
-    handleSetShowHome
-  } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, View, Text, StyleSheet } from "react-native";
+import DefaultButton from "../../Components/Common/DefaultButton";
+import ExplanationCard from "../../Components/Explanation/ExplanationCard";
 
-export default function appExplanation() {
-    function handleSetShowHome(){
-            console.log("Botão clicado");
-    }
+export default function AppExplanation() {
+  function handleSetShowHome() {
+    console.log("Botão clicado");
+  }
 
   return (
     <View style={styles.container}>
       <ScrollView>
         <View style={{ alignItems: "center" }}>
-          <Text style={styles.title}>Ante, deixa {"\n"} eu te explicar...</Text>
+          <Text style={styles.title}>
+            Antes, deixa {"\n"} eu te explicar...
+          </Text>
+          <ExplanationCard />
           <Text style={styles.descriptionCta}>
-            Pronto(a) para subir de nível na vida?
+            Pronto(a) para subir de nível na vida? {"\n"}
           </Text>
           <Text style={styles.description}>
             Na próxima tela você vai poder escolher {"\n"} seus 4 hábitos
-            individual.
+            individual.{"\n"}
           </Text>
           <DefaultButton
             buttonText={"Continuar"}
             handlePress={handleSetShowHome}
             width={250}
-            heigt={50}
+            heigth={50}
           />
         </View>
       </ScrollView>
@@ -38,29 +35,28 @@ export default function appExplanation() {
   );
 }
 
-const styles= StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "rgba(21, 21, 21, 00.98)",
-    },
-    title:{
-        fontSize: 30,
-        fontWeigth: "bold",
-        color: "whithe",
-        textAlign: "center",
-        marginVertical: 40, 
-    },
-    descriptionCta:{
-        color: "whithe",
-        fontWeigth: "bold",
-        fontSize: 16,
-        marginTop: 20,
-        marginBotton: 10,
-    },
-    description: {
-        color: "white",
-        textAlign:"center",
-        marginBotton: 30,
-    },
-
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 00.98)",
+  },
+  title: {
+    fontSize: 30,
+    fontWeigth: "bold",
+    color: "white",
+    textAlign: "center",
+    marginVertical: 40,
+  },
+  descriptionCta: {
+    color: "white",
+    fontWeigth: "bold",
+    fontSize: 16,
+    marginTop: 20,
+    marginBotton: 10,
+  },
+  description: {
+    color: "white",
+    textAlign: "center",
+    marginBotton: 30,
+  },
+});
